ec764e3cbfcd6b93804106254aee1b03f136d41b
fangqiank <fangqiank@msn.com>
2024-07-02 14:45:11 +0800

    before deploy

--- components/shared/Category.tsx
+++ components/shared/Category.tsx
@@ -45,10 +45,7 @@ export const Category = ({category}: CategoryProps) => {
 						'h-2 w-full',
 						progressBg
 					)}
-					indicatorClassName={cn(
-						"h-2 w-full", 
-						indicator
-					)}
+					
 				/>
 			</div>
 		</div>
